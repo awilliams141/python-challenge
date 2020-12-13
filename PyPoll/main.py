bd5721aa018abfaf9e8d9618342a8e9f170d34a1
austin williams <awilliams141@austins-MacBook-Pro.local>
2020-12-12 21:48:50 -0500

    PyPoll analysis

---
+++ PyPoll/main.py
@@ -0,0 +1,57 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+
+    total = 0
+    khanVotes = 0
+    correyVotes = 0
+    liVotes = 0
+    otoolVotes = 0
+
+    for row in csvreader:
+        total += 1
+        if row[2] == "Khan":
+            khanVotes += 1
+        elif row[2] == "Correy":
+            correyVotes += 1
+        elif row[2] == "Li":
+            liVotes += 1
+        elif row[2] == "O'Tooley":
+            otoolVotes += 1
+
+        if khanVotes > correyVotes and khanVotes > liVotes and khanVotes > otoolVotes:
+            winner = "Khan"
+        elif correyVotes > khanVotes and correyVotes > liVotes and correyVotes > otoolVotes:
+            winner = "Correy"
+        elif liVotes > khanVotes and liVotes > correyVotes and liVotes > otoolVotes:
+            winner = "Li"
+        elif otoolVotes > khanVotes and otoolVotes > correyVotes and otoolVotes > liVotes:
+            winner = "O'Tooley"
+
+    khanPer = (khanVotes / total) * 100
+    correyPer = (correyVotes / total) * 100
+    liPer = (liVotes / total) * 100
+    otoolPer = (otoolVotes / total) * 100
+    
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total}")
+    print("-------------------------")
+    print(f"Khan: {round(khanPer, 3)}% ({khanVotes})")
+    print(f"Correy: {round(correyPer, 3)}% ({correyVotes})")
+    print(f"Li: {round(liPer, 3)}% ({liVotes})")
+    print(f"O'Tooley: {round(otoolPer, 3)}% ({otoolVotes})")
+    print("-------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------")
